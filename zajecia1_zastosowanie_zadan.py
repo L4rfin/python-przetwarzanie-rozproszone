207b7d1ee36605e957c4d2e8dcc5eafab7cdd1ce
L4rfin <adrian.zajac.it@wp.pl>
2024-02-20 17:19:39 +0100

    zajecia1

---
+++ zajecia1_zastosowanie_zadan.py
@@ -0,0 +1,16 @@
+from zajecia1 import zadanie_1
+from zajecia1 import zadanie_2
+from zajecia1 import zadanie_3
+from zajecia1 import zadanie_4
+from zajecia1 import zadanie_5
+from zajecia1 import zadanie_6
+from zajecia1 import zadanie_7
+from zajecia1 import zadanie_8
+from zajecia1 import zadanie_9
+from zajecia1 import zadanie_10
+
+# zadanie 1 predefiniowane funkcje
+zadanie_1.invoke_functions()
+
+# zadanie 2 wczytanie funkcji z innego pliku
+zadanie_2.invoke_functions()
